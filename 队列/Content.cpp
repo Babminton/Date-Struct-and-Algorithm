9d5231aa6e0ba16d96ce5f64aa2c847d7497d6d6
Babminton <zengxiangqu@163.com>
2018-11-05 16:03:31 +0800

    队列的顺序存储结构-环形队列-基本操作

---
+++ 队列/Content.cpp
@@ -0,0 +1,27 @@
+﻿#include "SqQueue.h"
+
+int main() {
+
+	SqQueue q;
+	InitQueue(q);
+	for (int i = 1; i <= 10; i++) {
+		EnQueue(q,i);
+	}
+	cout << "环形队列的长度" << QueueLength(q) << endl;
+	ElemType x;
+	DeQueue(q, x);
+	cout<<"此时出队的是："<<x<<endl;
+	ElemType y;
+	GetFront(q, y);
+	cout<<"此时的队头是: "<<y<<endl;
+
+	cout << "遍历环形队列: ";
+	TraverseQueue(q);
+
+
+	system("pause");    //程序终止设置
+	return 0;
+
+}
+
+
